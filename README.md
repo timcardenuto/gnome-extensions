aaf845846754314866cf162ed3432904364f2c28
Tim Cardenuto <timcardenuto@gmail.com>
2018-05-04 10:50:53 -0400

    adding rest/websockets examples

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 
-# Fix gnome-shell-extensions-tool for CentOS 7
+### Fix gnome-shell-extensions-tool for CentOS 7
 
     sudo yum install python34 python34-pip python34-gobject
     sudo python3 -m pip install gi
@@ -10,7 +10,7 @@
       #!/usr/bin/python3
 
 
-# Generate your hello-world extension skeleton 
+### Generate your hello-world extension skeleton 
 
     $ gnome-shell-extensions-tool --create-extension
     
@@ -32,15 +32,22 @@
 	use an extension title clicktofocus@janedoe.example.com.
 	Uuid [stoplight@localhost.localdomain]: 
 
-# Enable extension
+
+This actually creates the skeleton project directly under `~/.local/share/gnome-shell/extensions`. You'll need to edit it there or copy somewhere and remember to copy back when you want to update the live version.
+
+
+### Enable extension
 
     gsettings set org.gnome.shell enabled-extensions "['stoplight@localhost.localdomain']"
 
-Press Alt-F2, and then enter 'r' to reload gnome extensions. You should now see some gears at the top right toolbar, if you click "Hello, world!" will pop up in the center of the screen. 
+Press Alt-F2, and then enter 'r' to reload gnome extensions. You should now see some gears at the top right toolbar, if you click "Hello, world!" will pop up in the center of the screen. Everytime you make code changes you can copy the new files to `~/.local/share/gnome-shell/extensions` if they're not already there, and reload the extensions.
 
 
-# Hack time
+### Hack time
 
-
+Find more examples of stuff
+ * http://mathematicalcoffee.blogspot.com/2012/11/sending-notifications-in-gnome-shell.html
+ * https://github.com/hackedbellini/Gnome-Shell-Notifications-Alert
+ * https://www.abidibo.net/blog/2016/03/02/how-i-developed-my-first-gnome-shell-extension/
 
 
--- stoplight@localhost.localdomain/extension.js
+++ stoplight@localhost.localdomain/extension.js
@@ -2,6 +2,8 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
+const Lang = imports.lang;
+const Soup = imports.gi.Soup	// REST and Websocket libraries
 
 let text, button;
 
@@ -11,8 +13,15 @@ function _hideHello() {
 }
 
 function _showHello() {
+	// pop-up at top of screen
     Main.notify('notification title', 'notification summary');    
 
+	// change icon color
+	let icon = new St.Icon({ icon_name: 'yellow',
+                             style_class: 'system-status-icon' });
+    button.set_child(icon);
+
+	// pop-up in middle of screen
     if (!text) {
         text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
         Main.uiGroup.add_actor(text);
@@ -30,6 +39,7 @@ function _showHello() {
                        time: 2,
                        transition: 'easeOutQuad',
                        onComplete: _hideHello });
+    //websocket.close();
 }
 
 function init(extensionMeta) {
@@ -46,7 +56,7 @@ function init(extensionMeta) {
                           track_hover: true });
 	
     // Edited icon to be green.svg on start
-    let icon = new St.Icon({ icon_name: 'red',
+    let icon = new St.Icon({ icon_name: 'green',
                              style_class: 'system-status-icon' });
 
     button.set_child(icon);
@@ -56,6 +66,8 @@ function init(extensionMeta) {
 
     //TODO need to add event listener here that changes colors ...
     //TODO ... and also makes popup like the _showHello, but persists until user clicks OK
+	//testREST();
+	testWebSocket();
 }
 
 function enable() {
@@ -65,3 +77,88 @@ function enable() {
 function disable() {
     Main.panel._rightBox.remove_child(button);
 }
+
+//--REST/Websocket stuff---------------------------------------------
+
+function testREST() {
+/*  // GET example
+    let params = {
+       amount: '1000',
+       sourceCurrency: 'CHF',
+       targetCurrency: 'EUR'
+    };
+    let message = Soup.form_request_new_from_hash('GET', 'http://localhost:8000/geolocation', params);
+    message.request_headers.append("Content-type", "application/json");
+*/
+    let message = Soup.Message.new('GET', 'http://localhost:8000/geolocation')
+
+	// execute the request and define the callback
+	let _httpSession = new Soup.Session();
+	_httpSession.queue_message(message, Lang.bind(this,
+		function (_httpSession, message) {
+			if (message.status_code !== 200) {
+				// change icon color
+				let icon = new St.Icon({ icon_name: 'red',
+								  style_class: 'system-status-icon' });
+				button.set_child(icon);
+				return;
+			}
+			Main.notify('Websocket', 'response: ' + message.response_body.data);
+			let json = JSON.parse(message.response_body.data);
+			// do something with the data, check for stuff
+
+		})
+	);
+}
+
+function testWebSocket() {
+
+    let message = Soup.Message.new('GET', 'ws://localhost:9000/ws')
+
+	let _httpSession = new Soup.Session();
+	_httpSession.httpAliases = ["ws"];
+	_httpSession.websocket_connect_async(message, null, null, null, Lang.bind(this,
+		function (session, res) {
+			this._websocketConnection = session.websocket_connect_finish(res);
+	
+			this._websocketConnection.connect("message", Lang.bind(this, function(connection, type, message) {
+			    var data = JSON.parse(message.get_data());
+				Main.notify('Websocket', 'response: ' + message.get_data());
+				// do something with the data, check for stuff
+			}));
+ 		}
+	));
+
+
+	// close the socket
+	this._websocketConnection.close(Soup.WebsocketCloseCode.NORMAL, "");
+    Main.notify('Websocket', 'Closed');    
+/*
+    websocket = Soup.Websocket.Connection.new("ws://localhost:9000/ws");
+    websocket.onopen = function(evt) { onOpen(evt) };			// callbacks?
+    websocket.onclose = function(evt) { onClose(evt) };
+    websocket.onmessage = function(evt) { onMessage(evt) };
+    websocket.onerror = function(evt) { onError(evt) };
+*/
+}
+
+function onOpen(evt) {
+    Main.notify('Websocket', 'Connected');    
+    doSend("WebSocket rocks");
+}
+
+function onClose(evt) {
+    Main.notify('Websocket', 'Closed');    
+}
+
+function onMessage(evt) {
+    var msg = evt.data;
+    Main.notify('Websocket', msg);
+}
+
+function onError(evt) {
+    var err = evt.data;
+    Main.notify('Websocket', err);
+}
+
+
